090e15fc059f096e33fa76eea14f2f071f20059d
Makeda LSY <makeda.louisy@gmail.com>
2016-03-20 16:02:36 +0100

    Modification du fichier js/carre.js pour afficher le carré de tous les nombres entre 0 et 10.

--- js/carre.js
+++ js/carre.js
@@ -3,6 +3,8 @@ function carre(x) {
   return x*x;
 }
 
-console.log(carre(0)); // Doit afficher 0
-console.log(carre(2)); // Doit afficher 4
-console.log(carre(5)); // Doit afficher 25
+var nombre;
+
+for (nombre = 0; nombre<=10; nombre++) {
+    console.log(carre(nombre)); 
+}
